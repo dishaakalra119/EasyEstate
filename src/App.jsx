592ba970dbce6db7185bebef2a80ae54000e8cd3
dishaakalra119 <dishaakalra@gmail.com>
2025-09-23 22:37:24 +0530

    install react js with vite and tailwind and created first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline italic text-red-800">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
