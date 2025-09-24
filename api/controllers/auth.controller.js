72845c7e05f6a9580e2be84f27c9b2d4fe7d4652
dishaakalra119 <dishaakalra@gmail.com>
2025-09-24 05:38:52 +0530

    signup api route created commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,5 +9,5 @@ export const signup = async (req,res)=>{
         res.status(201).json("User created successfully");
     }catch(error){
         res.status(500).json(error.message);
-    }
+    };
 };
--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{console.log("Conected to database successfully")}).catch((err)=>{console.log(err)});
 const app=express();
