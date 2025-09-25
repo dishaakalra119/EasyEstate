eee8f6cb8c626baa50303ec3b9e2ddb0d161c098
dishaakalra119 <dishaakalra@gmail.com>
2025-09-25 11:09:45 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import {useRef} from 'react'
 import {useSelector} from 'react-redux'
 import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess,deleteUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 export default function Profile() {
   const fileRef=useRef(null);
@@ -75,6 +75,22 @@ export default function Profile() {
     }
   }
 
+  const handleDeleteUser=async()=>{
+    try{
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data= await res.json();
+      if(data.success===false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    }catch(error){
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -100,7 +116,7 @@ export default function Profile() {
         <button disabled={loading}className='bg-slate-700 text-white p-3 rounded-xl uppercase hover:opacity-95 disabled:opacity-80'>{loading?'Loading':'Update'}</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
       <p className='text-red-700 mt-5'>{error?error:''}</p>
